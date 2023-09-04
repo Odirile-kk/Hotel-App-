0c5706fc7de8cc00817da56e7e27f7c50a343a56
Odirile-kk <kekanaodirile@gmail.com>
2023-09-04 13:21:05 +0200

    firebase

---
+++ client/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCNi-QTH5YUlaZAzaaiAVIoLXxKoLo2fjw",
+  authDomain: "hotelapp-25306.firebaseapp.com",
+  projectId: "hotelapp-25306",
+  storageBucket: "hotelapp-25306.appspot.com",
+  messagingSenderId: "160859528659",
+  appId: "1:160859528659:web:5db07c4c4e665f323fa326"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
